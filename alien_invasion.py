59a97c2b31dba50157114ae51b9c37869eb69274
chikwongyip <527439024@qq.com>
2022-08-18 16:56:05 +0800

    pygame_18

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,25 +2,28 @@ import sys
 import pygame
 import game_functions as gf
 from setting import Settings
-from ship import Ship
+from ship import Ship,Boss
+
 def run_game():
     """初始化游戏"""
     pygame.init()
     ai_setting = Settings()
     screen = pygame.display.set_mode((ai_setting.screen_width,ai_setting.screen_height))
     pygame.display.set_caption("Alien Invasion")
+    pygame.image
     ship = Ship(screen)
+    boss = Boss(screen)
     """开始游戏"""
     while True:
-        gf.check_events()
+        gf.check_events(ship)
         gf.update_screen(ai_setting, screen, ship)
         screen.fill(ai_setting.bg_color)
         ship.blitme()
-        ship.blitme()
+        boss.blitme()
         # 监视键盘和鼠标事件
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
+        # for event in pygame.event.get():
+        #     if event.type == pygame.QUIT:
+        #         sys.exit()
         
         pygame.display.flip()
 
