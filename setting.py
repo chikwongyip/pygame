820c32153e34809db6dd7145dda66ca05510d78f
chikwongyip <527439024@qq.com>
2022-08-22 16:16:26 +0800

    20220822-2

--- setting.py
+++ setting.py
@@ -7,4 +7,9 @@ class Settings():
         self.screen_height = 800
         self.bg_color = (0,0,255)
         self.ship_speed_factor = 1.5
+        #子弹设置
+        self.bullet_speed_factor = 1
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = 60,60,60
 
